7f68fc71ff8a1ed9a326dcad80d8696ac28b495c
jkzhong <jkzhong429@gmail.com>
2014-01-18 00:02:48 +0800

    insert dtat to fb First version finish

--- Proj_dev_code/MongoDB/Process_table_Fn.py
+++ Proj_dev_code/MongoDB/Process_table_Fn.py
@@ -1,15 +1,84 @@
+import csv
 import pymongo
 #client = pymongo.MongoClient("localhost", 27017)
 client = pymongo.MongoClient("mongodb://www.bi-xi.com:27017")
 db = client.Taiwan_Stock
 
-def creat_company_db(collection_name,company_number,company_name,file_data):
+#==========================================================#
+#Format1 #
+#==========================================================#
+def Format1_creat_company_db(collection_name,company_number,company_name,file_data):
     db[collection_name].insert({        #creat company object
         "_id":company_number,
         "com_name":company_name,
     })
     for row in file_data[:-1]:
         if row != "" :
+            #print(row)
+            items = row.split(",")
+            db[collection_name].update(
+                {"_id":company_number}, #find target
+                {"$push" :{items[0]:items[1:-1]}}  #late item is empty; update target
+                #{upsert:True}
+            )        
+    return 0
+    
+def Format1_updata_company_db(collection_name,company_number,company_name,file_data):
+    #input("wait key update")
+    return 0
+#==========================================================#
+#Format2 #
+#==========================================================#
+def Format2_creat_company_db(collection_name,company_number,company_name,file_data,file_path):
+    db[collection_name].insert({        #creat company object
+        "_id":company_number,
+        "com_name":company_name,
+    })
+    #for row in csv.DictReader(file_data):
+    num_lines = sum(1 for line in open(file_path))
+    item_number = len(file_data[0].split(",")) - 1    #skip late enpty item
+    temp_data = [[None for x in range(num_lines)] for y in range(item_number)]    
+    #print(temp_data)
+    #input("wait input")
+    i=0
+    j=0
+    for row in file_data:
+        if row != "" :
+            j=0
+            for item in row.split(","):
+                if item !="":
+                    #print(item)
+                    temp_data[j][i] = item
+                    j=j+1
+            #print(row)
+            #print(temp_data)
+            #input("wait key")
+            i=i+1
+            
+    for line_data in temp_data:
+        print(line_data)
+        db[collection_name].update(
+            {"_id":company_number}, #find target
+            {"$push" :{line_data[0]:line_data[1:-1]}}  #late item is empty; update target
+            #{upsert:True}
+        )        
+        #input ("wait key")        
+    return 0
+    
+def Format2_updata_company_db(collection_name,company_number,company_name,file_data,file_path):
+    #input("wait key update")
+    return 0     
+    
+#==========================================================#
+#Format3 #
+#==========================================================#
+def Format3_creat_company_db(collection_name,company_number,company_name,file_data):
+    db[collection_name].insert({        #creat company object
+        "_id":company_number,
+        "com_name":company_name,
+    })
+    for row in file_data[:-1]:
+        if ( (row != "") & (len(row.split(","))>2)):
             print(row)
             items = row.split(",")
             db[collection_name].update(
@@ -19,6 +88,96 @@ def creat_company_db(collection_name,company_number,company_name,file_data):
             )        
     return 0
     
-def updata_company_db(collection_name,company_number,company_name,file_data):
+def Format3_updata_company_db(collection_name,company_number,company_name,file_data):
     #input("wait key update")
     return 0
+    
+#==========================================================#
+#Format4 #
+#==========================================================#
+def Format4_creat_company_db(collection_name,company_number,company_name,file_data):
+    db[collection_name].insert({        #creat company object
+        "_id":company_number,
+        "com_name":company_name,
+    })
+    for row in file_data[:-1]:
+        if (row != ""):
+            print(row)
+            items = row.split(",")
+            if (items[0]!=""):
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[0]:items[1:-1]}}  #late item is empty; update target
+                    #{upsert:True}
+                )        
+    return 0
+    
+def Format4_updata_company_db(collection_name,company_number,company_name,file_data):
+    #input("wait key update")
+    return 0
+    
+    
+#==========================================================#
+#Format5 #
+#==========================================================#
+def Format5_creat_company_db(collection_name,company_number,company_name,file_data):
+    db[collection_name].insert({        #creat company object
+        "_id":company_number,
+        "com_name":company_name,
+    })
+    i=1
+    for row in file_data[:-1]:        
+        if (row != ""):
+            items = row.split(",")
+            if(i==1 or i==3):    
+                print(row)      
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[0]:items[1]}}  #late item is empty; update target
+                )        
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[2]:items[3]}}  #late item is empty; update target
+                )        
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[4]:items[5]}}  #late item is empty; update target
+                )        
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[6]:items[7]}}  #late item is empty; update target
+                )        
+            if(i==2 or i==4 or i==5 or i==7 or i==8):
+                print(row)      
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[0]:items[1]}}  #late item is empty; update target
+                )        
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[2]:items[3]}}  #late item is empty; update target
+                )        
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[4]:items[5]}}  #late item is empty; update target
+                )        
+            if(i==9 or i==10 or i==11):
+                print(row)      
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[0]:items[1]}}  #late item is empty; update target
+                )        
+                db[collection_name].update(
+                    {"_id":company_number}, #find target
+                    {"$push" :{items[2]:items[3]}}  #late item is empty; update target
+                )        
+        i=i+1 #next row
+    return 0
+    
+def Format5_updata_company_db(collection_name,company_number,company_name,file_data):
+    #input("wait key update")
+    return 0
+    
+    
+    
+    
--- Proj_dev_code/MongoDB/insert_data2db.py
+++ Proj_dev_code/MongoDB/insert_data2db.py
@@ -26,28 +26,11 @@ for folders in os.listdir(lasted_data):
     
     
     #=============================================================================#
-    #below data table are same as cash_flow table; use cash flow functions        #
+    #below data table are same as Fomat1    #
     #=============================================================================#
     
     if folders == "cash_flow" :
-        db.cash_flow
-        for files in os.listdir(lasted_data+"/%s"%folders):
-            #print(files)
-            file_path = lasted_data+"/%s"%folders+"/%s"%files            
-            file_data = open(file_path,"r")
-            company_number = files[:4]
-            company_name   = files[5:-4]
-            #print(db.collection_names())P
-            print(company_number)
-            company_exist = db.cash_flow.find({"_id":company_number}).count()
-            if company_exist !=0 : #updata
-                Process_table_Fn.updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
-            else : #creat 
-                Process_table_Fn.creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
-            file_data.close()
-    
-    if folders == "balance_sheet" :
-        db.balance_sheet
+        db[folders]
         for files in os.listdir(lasted_data+"/%s"%folders):
             #print(files)
             file_path = lasted_data+"/%s"%folders+"/%s"%files            
@@ -56,10 +39,184 @@ for folders in os.listdir(lasted_data):
             company_name   = files[5:-4]
             #print(db.collection_names())
             print(company_number)
-            company_exist = db.balance_sheet.find({"_id":company_number}).count()
+            company_exist = db[folders].find({"_id":company_number}).count()
             if company_exist !=0 : #updata
-                Process_table_Fn.updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+                Process_table_Fn.Format1_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
             else : #creat 
-                Process_table_Fn.creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+                Process_table_Fn.Format1_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
             file_data.close()
+    
+    if folders == "balance_sheet" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format1_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            else : #creat 
+                Process_table_Fn.Format1_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            file_data.close()
+            
+    if folders == "quarter_income_sheet" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format1_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            else : #creat 
+                Process_table_Fn.Format1_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            file_data.close()
+            
+    #=============================================================================#
+    #below data table are same as Fomat2    #
+    #=============================================================================#
+    if folders == "monthly_revenue_no_merge" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format2_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            else : #creat 
+                Process_table_Fn.Format2_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            file_data.close()
+   
+    if folders == "quarter_dividend_merge" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format2_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            else : #creat 
+                Process_table_Fn.Format2_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            file_data.close()
+            
+    if folders == "quarter_profit_merge" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format2_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            else : #creat 
+                Process_table_Fn.Format2_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            file_data.close()
+            
+    if folders == "quarter_revenue_no_merge" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format2_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            else : #creat 
+                Process_table_Fn.Format2_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"),file_path)
+            file_data.close()
+        
+    #=============================================================================#
+    #below data table are same as Fomat3    # format 1 like;  row table format
+    #=============================================================================#        
+    
+    if folders == "quarter_financial_ratio_sheet" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format3_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            else : #creat 
+                Process_table_Fn.Format3_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            file_data.close()
+    
+    
+    #=============================================================================#
+    #below data table are same as Fomat4    # format 1 like;  row table format
+    #=============================================================================#        
+    
+    if folders == "shareholding_distribution" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format4_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            else : #creat 
+                Process_table_Fn.Format4_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            file_data.close()
+            
+    #=============================================================================#
+    #below data table are same as Fomat4    # format 1 like;  row table format
+    #=============================================================================#        
+    
+    if folders == "basic_information" :
+        db[folders]
+        for files in os.listdir(lasted_data+"/%s"%folders):
+            #print(files)
+            file_path = lasted_data+"/%s"%folders+"/%s"%files            
+            file_data = open(file_path,"r")
+            company_number = files[:4]
+            company_name   = files[5:-4]
+            #print(db.collection_names())
+            print(company_number)
+            company_exist = db[folders].find({"_id":company_number}).count()
+            if company_exist !=0 : #updata
+                Process_table_Fn.Format5_updata_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            else : #creat 
+                Process_table_Fn.Format5_creat_company_db(folders,company_number,company_name,file_data.read().split("\n"))
+            file_data.close()
+    
+    
+    
+    
+    
     
